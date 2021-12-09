28f2ea842ae8baeaf692bd0eb3419a7e444c1094
Wassana Sengsavang <wassana.seng@gmail.com>
2021-12-09 14:20:21 +0100

    Home kickstart

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  routes to: 'lists#index'
   resources :lists, only: [ :index, :show, :new, :create ] do
     resources :bookmarks, only: [ :new, :create ]
   end
